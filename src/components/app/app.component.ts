51814f956f38fa3f332b6035006562ef0d58eb4a
Webira <guery.ira@gmail.com>
2024-06-27 14:15:46 +0200

    change br

--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { NavComponent } from '../nav/nav.component';
+import { TaskComponent } from '../task/task.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , HeaderComponent, FooterComponent, NavComponent],
+  imports: [RouterOutlet , HeaderComponent, FooterComponent, NavComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
