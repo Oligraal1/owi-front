bbc68a3e78772521d55d8f5252c5523b21f525d1
Wwwided <widedibcegos@gmail.com>
2024-06-27 11:58:27 +0200

    ajout fetcher et affichage projects local nav

--- src/components/nav/nav.component.ts
+++ src/components/nav/nav.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FetcherService } from '../../services/fetcher.service';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  projects: any = [];
+  constructor(public fetcher: FetcherService) {}
+
+  ngOnInit(): void {
+    /*this.fetcher.getProjects().subscribe((data:any) => {
+      console.log(data);
+      this.projects = data;
+    })*/
+   this.projects = ["projet1","projet2","projet3"]; //ligne à supprimer après lien avec API
+  }
 
 }
