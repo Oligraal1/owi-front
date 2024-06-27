a4ec7441adc4b89df18743ab0de9cf9beeb63295
HollyWeb <46522066+Oligraal1@users.noreply.github.com>
2024-06-27 11:00:56 +0200

    fixe merge

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ListingComponent } from './components/listing/listing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch:'full'},
+  {path: 'home', component: ListingComponent},];
