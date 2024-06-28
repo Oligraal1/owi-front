7a8c727ff378e12bb7fc4fcf307b1c1aaa792557
Wwwided <widedibcegos@gmail.com>
2024-06-28 14:49:05 +0200

    form creer projet

--- src/components/nav/nav.component.ts
+++ src/components/nav/nav.component.ts
@@ -1,16 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { FetcherService } from '../../services/fetcher.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
 export class NavComponent implements OnInit {
   @Input() projects: any = [];
+  newProjectName: string = '';
   constructor(public api: FetcherService) {}
 
   ngOnInit(): void {
@@ -18,7 +20,19 @@ export class NavComponent implements OnInit {
       console.log(data);
       this.projects = data;
     })
-  
+  }
+
+  toCreateProject(){
+    const newProject = {name: this.newProjectName};
+    
+    this.api.createProject(newProject).subscribe(
+      () => {
+        console.log('Nouveau projet créé avec succès');
+      },
+      (error) => {
+        console.error('Erreur lors de la création du projet', error);
+      }
+    );
   }
 
 }
