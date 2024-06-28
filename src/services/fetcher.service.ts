a39765e617c24562cbe968a1bfdcb01e7377a4e5
Wwwided <widedibcegos@gmail.com>
2024-06-28 10:39:56 +0200

    project lien api local ok

--- src/services/fetcher.service.ts
+++ src/services/fetcher.service.ts
@@ -8,18 +8,30 @@ import { Observable } from "rxjs";
 export class FetcherService {
     constructor(public http: HttpClient) {}
     /*
-    private apiUrl = "https://owi-back.azurewebsites.net"
-
-    getProjects(){
-    return this.http.get(apiUrl + '/Projects');
-    }
+    private apiUrl = "https://owi-back.azurewebsites.net"}
     */
    private apiUrl = "http://localhost:5291/api";
 
    //PROJECTS
-   getProjects() {
-    return this.http.get(this.apiUrl+"/projects");
-   }
+  getProjects(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/projects`);
+  }
+ 
+  getProjectById(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/projects/${id}`);
+  }
+
+  createProject(project: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/projects`, project);
+  }
+
+  updateProject(id: number, project:any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/projects/${id}`, project);
+  }
+
+  deleteProject(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/projects/${id}`);
+  }
 
    //LISTING
    getListingsByProjectId(projectId: number): Observable<any[]> {
